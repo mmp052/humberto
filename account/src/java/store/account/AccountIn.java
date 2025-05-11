458939bf5835065915d32a0a9358793f3410cc8b
mmp052 <mateusmoreirammp052@gmail.com>
2025-05-11 18:47:36 -0300

    primeiro

---
+++ account/src/java/store/account/AccountIn.java
@@ -0,0 +1,14 @@
+
+package store.account;
+
+import lombok.Builder;
+import lombok.experimental.Accessors;
+
+@Builder @Accessors(fluent = true)
+public record AccountIn(
+    String name,
+    String email,
+    String password
+) {
+
+}
